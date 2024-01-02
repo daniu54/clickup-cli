0dcb31077aff93d0c8381d722bdd2f811acd6c7b
daniu54 <daniu.coding@proton.me>
2024-01-02 09:54:52 +0000

    ran code formatter

--- src/clickup/clickup_api.py
+++ src/clickup/clickup_api.py
@@ -137,7 +137,10 @@ def find_tasks_in_workspace(
         task_id = simplify_string(task["id"])
 
         if filter in task_id or filter in task_name:
-            print(f"Found match for '{filter}' task id: '{task_id}' task name: '{task_name}'", file=sys.stderr)
+            print(
+                f"Found match for '{filter}' task id: '{task_id}' task name: '{task_name}'",
+                file=sys.stderr,
+            )
             matches.append(task)
 
     return matches
